10f18372070cf23f7aa062f95d3f7948384c9c47
dema121 <andrea.dematte@gmail.com>
2019-09-20 11:44:33 +0200

    Renamed injected test files

--- controllers/ctrlWebProxy.js
+++ controllers/ctrlWebProxy.js
@@ -7,9 +7,9 @@ const manageRequest = async function(req, res) {
     responseText = injectBaseUrl(responseText, baseUrl)
     responseText = injectScript(req, responseText, "/resources/libs/jquery/jquery.min.js")
     responseText = injectScript(req, responseText, "/resources/libs/annotatorjs/annotator.min.js")
-    responseText = injectScript(req, responseText, "/resources/scripts/test.js")
+    responseText = injectScript(req, responseText, "/resources/scripts/testInject.js")
     responseText = injectScript(req, responseText, "/resources/scripts/ctrl-highlight-answers.js")
-    responseText = injectCss(req, responseText, "/resources/css/test.css")
+    responseText = injectCss(req, responseText, "/resources/css/testInject.css")
     
     res.send(responseText)
 
