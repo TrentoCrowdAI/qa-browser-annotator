5b3a6992d9c05cf9421a8cfb95ae73c5a889e2c9
dema121 <andrea.dematte@gmail.com>
2019-09-27 11:12:45 +0200

    Webpage selector UI

--- public/scripts/ui-main.js
+++ public/scripts/ui-main.js
@@ -1,11 +1,37 @@
 $(function() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const pageurl = urlParams.get('url');
+    let urlParams = new URLSearchParams(window.location.search);
+    let pageurl = urlParams.get('url');
 
     const webpageRenderer = $("#webpage-renderer");
+    const inputWebpageUrl = $("#input-webpage-url");
+    const btnWebpageLoad = $("#btn-webpage-load");
     
-    let annotatorUrl = "/annotator?url=" + pageurl + "&requestType=webproxy";
-    webpageRenderer.attr("data", annotatorUrl);
-    console.log(annotatorUrl);
+    
+    btnWebpageLoad.click(function () {
+        insertUrlParam(urlParams, "url", inputWebpageUrl.val());
+        pageurl = urlParams.get('url');
+        initUI();
+    });
+    
+    if (pageurl) {
+        inputWebpageUrl.val(pageurl);
+        initUI();
+    }
+
+    function initUI() {
+        if (!pageurl) return;
+        let annotatorUrl = "/annotator?url=" + pageurl + "&requestType=webproxy";
+        webpageRenderer.attr("data", annotatorUrl);
+        console.log(annotatorUrl);
+    }
 
 });
+
+
+function insertUrlParam(urlParams, key, value) {
+    if (history.pushState) {
+        urlParams.set(key, value);
+        let newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + '?' + urlParams.toString();
+        window.history.replaceState({path: newurl}, '', newurl);
+    }
+}
